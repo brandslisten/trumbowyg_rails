7e5f38f184b12b1752315fc9fa7e99f59d37c97c
Sven Dunemann <dunemann@forelabs.eu>
2018-02-27 14:20:23 +0100

    Use svg icons

--- vendor/assets/javascripts/trumbowyg/vendor/plugins/contextbar/trumbowyg.contextbar.js
+++ vendor/assets/javascripts/trumbowyg/vendor/plugins/contextbar/trumbowyg.contextbar.js
@@ -3,7 +3,7 @@
  * Contextbar plugin for Trumbowyg
  * http://alex-d.github.com/Trumbowyg
  * ===========================================================
- * Author : Sven Dunemann
+ * Author : Sven Dunemann [dunemann@forelabs.eu]
  */
 
 (function ($) {
@@ -13,11 +13,11 @@
       hideBtnPane: false,
       sidePane: false,
       btns: {
-        side: ['p','blockquote','h2','h3','h4','createLink','upload','insertImage','insertImageFromGallery','justifyLeft','justifyCenter','justifyRight','justifyFull','table','unorderedList','orderedList','horizontalRule'],
+        side: ['p','blockquote','h2','h3','h4','internalLink','createLink','upload','insertImage','insertImageFromGallery','justifyLeft','justifyCenter','justifyRight','justifyFull','table','unorderedList','orderedList','horizontalRule'],
         text: ['p','blockquote','h2','h3','h4','strong','em','underline','createLink','justifyLeft','justifyCenter','justifyRight','justifyFull','unorderedList','orderedList'],
         table: ['tableAddRow','tableAddColumn','tableDeleteRow','tableDeleteColumn','tableDestroy'],
         a: ['editLink','unlink'],
-        img: ['editImage','justifyLeft','justifyCenter','justifyRight']
+        img: ['editImage','justifyLeft','justifyCenter','justifyRight','deleteImage']
       },
       mapping: {
         td: 'table',
@@ -52,16 +52,27 @@
                   };
                   var btnDefEditImage = {
                     title: "Edit image",
-                    text: '<i class="fa fa-pencil" />',
+                    text: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="m505.99-87.12v3.12h3.12l7.88-7.88-3.12-3.12zm13.76-7.51c.33-.33.33-.85 0-1.18l-1.95-1.95c-.33-.33-.85-.33-1.18 0l-1.63 1.64 3.12 3.12z" transform="matrix(.85668 0 0 .85668-431.46 85.958)"/></svg>',
                     hasIcon: false,
                     fn: function() {
                       t.saveRange();
                       t.getDefaultImgDblClickHandler().bind(t.doc.getSelection().focusNode)();
                     }
                   };
+                  var btnDefDeleteImage = {
+                    title: "Delete image",
+                    text: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10"><g transform="matrix(0.37650603,0,0,0.37650603,-33.738875,-19.449912)"><g transform="matrix(0.94444444,0,0,0.94444444,5.7161365,3.6077199)"><path style="clip-rule:nonzero;display:inline;overflow:visible;visibility:visible;opacity:1;isolation:auto;mix-blend-mode:normal;color-interpolation:sRGB;color-interpolation-filters:linearRGB;solid-color:#000000;solid-opacity:1;fill:none;fill-opacity:1;fill-rule:nonzero;stroke:#00b393;stroke-width:1.98214173;stroke-linecap:butt;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;color-rendering:auto;image-rendering:auto;shape-rendering:auto;text-rendering:auto;enable-background:accumulate" d="M 114.04368,64.938966 A 11.153227,10.96093 0 0 1 102.89045,75.899895 11.153227,10.96093 0 0 1 91.737221,64.938966 11.153227,10.96093 0 0 1 102.89045,53.978037 11.153227,10.96093 0 0 1 114.04368,64.938966 Z" inkscape:connector-curvature="0" /><g style="fill-opacity:1;stroke:#00b393;stroke-width:23.34250641;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" transform="matrix(0.06345829,0,0,0.06345829,94.278428,54.840952)"><path style="fill-opacity:1;stroke:#00b393;stroke-width:22.72358704;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" d="m 75.849599,90.003696 c -1.495219,0 -2.973932,0.579096 -4.114132,1.719848 -2.281332,2.281328 -2.281332,5.980605 0,8.26195 L 115.77673,144.0268 c 1.13954,1.14024 2.61853,1.71986 4.11426,1.71986 1.49523,0 3.00667,-0.57962 4.14777,-1.71986 2.28133,-2.28134 2.28133,-5.9806 0,-8.26194 L 79.997436,91.723544 c -1.14058,-1.140752 -2.652617,-1.719848 -4.147837,-1.719848 z m 123.153881,0 c -1.49522,0 -3.00771,0.579096 -4.14794,1.719848 L 71.735467,214.87736 c -2.281332,2.28134 -2.281332,5.98061 0,8.26197 1.140235,1.14023 2.61855,1.71984 4.114132,1.71984 1.495566,0 3.00667,-0.57961 4.147837,-1.71984 L 203.11758,99.985494 c 2.28132,-2.282311 2.28132,-5.979633 0,-8.26195 -1.14128,-1.140752 -2.61889,-1.719848 -4.1141,-1.719848 z M 152.9388,168.13826 c -1.49522,0.0161 -2.98612,0.6012 -4.1141,1.75355 -2.25698,2.30569 -2.20363,6.00496 0.10356,8.26197 l 45.99717,45.01927 c 1.13781,1.11295 2.60626,1.68618 4.08042,1.68618 1.51594,0 3.0374,-0.58515 4.18143,-1.75357 2.25699,-2.30568 2.20365,-6.00496 -0.10355,-8.26196 l -45.99717,-45.01926 c -1.15336,-1.12901 -2.65289,-1.70224 -4.14777,-1.68618 z" inkscape:connector-curvature="0" /></g></g></g></svg>',
+                    hasIcon: false,
+                    fn: function() {
+                      if(t.doc.getSelection().focusNode.nodeName == 'IMG') {
+                        t.doc.getSelection().focusNode.remove();
+                      }
+                    }
+                  };
 
                   t.addBtnDef('editLink', btnDefEditLink);
                   t.addBtnDef('editImage', btnDefEditImage);
+                  t.addBtnDef('deleteImage', btnDefDeleteImage);
 
                   var isNullRect = function(rect) {
                     return rect.x == 0 &&
@@ -166,7 +177,6 @@
 
                       t.$box.append(side);
 
-                      // TODO: insert data on current cursor, not behind node
                       side.on('click', function(){
                         t.$box.append(pane);
                         side.remove();
